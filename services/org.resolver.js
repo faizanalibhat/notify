b44562fa0d754b5dd0843d8e4d3132559ae22395
pyloris <shoaibwani@protonmail.com>
2025-04-29 08:59:13 +0200

    fix: handled pagination with users endpoint

--- services/org.resolver.js
+++ services/org.resolver.js
@@ -1,18 +1,59 @@
 const axios = require("axios");
 
 
-const resolveMembers = async (orgId) => {
+const resolveAllMembers = async (orgId) => {
     try {
-        const response = await axios.get(`${process.env.AUTH_SERVICE_URL}/api/org/members?orgId=${orgId}`, {
+
+        const base = await axios.get(`${process.env.AUTH_SERVICE_URL}/api/org/members?orgId=${orgId}&page=1&limit=1`, {
             method: "GET",
             headers: { 'service-api-key': process.env.SERVICE_KEY }
         });
 
-        const users = response?.data;
+        let total = base?.data?.data?.total || 0;
+        let limit = 50;
+
+        let pages = Math.ceil(total/limit);
+
+        let allUsers = [];
+
+        while (pages > 0) {
+            try {
+                let resp = await axios.get(`${process.env.AUTH_SERVICE_URL}/api/org/members?orgId=${orgId}&page=${pages}&limit=${limit}`, {
+                    method: "GET",
+                    headers: { 'service-api-key': process.env.SERVICE_KEY }
+                });
+
+                pages -= 1;
+
+                // users
+                let users = resp?.data?.data?.members;
+
+                users = Array.isArray(users) ? users : [];
+
+                allUsers.push(...users);
+            }
+            catch(err) {
+                console.log(err.message);
+                pages -= 1;
+            }
+        }
+
+        return allUsers || [];
+    }
+    catch(err) {
+        console.log(err.message);
+        return [];
+    }
+}
+
+
+const resolveMembers = async (orgId) => {
+    try {
+        const users = await resolveAllMembers(orgId); 
 
         console.log("users: ", users);
 
-        if (!Array.isArray(users)) {
+        if (!Array.isArray(users?.data?.m)) {
             return { code: 400, status: "failed", message: "failed to resolve members" };
         }
 
