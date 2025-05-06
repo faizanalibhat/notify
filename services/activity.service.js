7c51d79c63c9ded9c23e70f8c94937d2590d0231
pyloris <shoaibwani@protonmail.com>
2025-05-06 08:15:41 +0200

    Added new endpoints to activity service

--- services/activity.service.js
+++ services/activity.service.js
@@ -42,15 +42,47 @@ const endpointWithMethodActionMap = {
     '/op/assets/exposure': {
         'GET': "Listed Exposures in ASM",
     },
-    '/op/report': {
-        'GET': "Listed Reports in ASM"
+    'op/assets/exposure/[0-9a-f]+?/safe': {
+        'POST': 'Marked An Exposure as Safe'
     },
+    'op/assets/exposure/[0-9a-f]+?': {
+        'PATCH': 'Edited Description & Severity of an Exposure'
+    },
+    '/op/report': {
+        'GET': "Listed Reports in ASM",
+        'POST': "Report Generated in ASM"
+    },
+    '/op/cmd/scans': {
+        'GET': "Listed Scans in ASM",
+        'POST': "Created a new scan in ASM"
+    },
+    '/op/cmd/scans/[0-9a-f]+?': {
+        'DELETE': "Deleted a scan in ASM"
+    },
+    '/op/assets/[a-z]+?/asset/[0-9a-f]+?/exposure': {
+        'POST': "Created an Exposure",
+    }
 }
 
 
 const parseActivity = (endpoint, method) => {
 
-    const activity = endpointWithMethodActionMap?.[endpoint]?.[method];
+    // get all regex keys
+    let keys = Object.keys(endpointWithMethodActionMap);
+
+    // find the key that matches the endpoint and use it to find the activity.
+    let matchingKey = keys.filter(key => {
+        let match = endpoint.match(`^${key}$`);
+
+        if (match && match?.length) {
+            return true;
+        }
+
+        return false;
+    });
+
+
+    const activity = endpointWithMethodActionMap?.[matchingKey]?.[method];
 
     return activity;
 
