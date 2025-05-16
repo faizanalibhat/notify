08fba59b93370ad83eaee718425254baa85eea20
pyloris <shoaibwani@protonmail.com>
2025-05-16 11:40:28 +0200

    added filters and search functionality

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -4,7 +4,7 @@ const notificationService = require("../services/notification.service");
 const getAllNotifications = async (req, res) => {
     const { orgId, email } = req.authenticatedService;
 
-    const { page=1, limit=10, origin, seen } = req.query;
+    const { page=1, limit=10, origin, seen, search } = req.query;
 
     const filter = {};
 
@@ -13,6 +13,19 @@ const getAllNotifications = async (req, res) => {
     filter['createdBy.email'] = { $ne: email };
 
     if (seen) filter.seen = seen == 'true';
+
+    if (search) {
+        filter.title = { $regex: search, $options: 'i' };
+        filter.description = { $regex: search, $options: 'i' };
+    }
+
+    for (let [key,val] of Object.entries(req.query)) {
+        if (!['page', 'limit', 'origin', 'seen', 'search'].includes(key)) {
+
+            if (key == 'product') filter['origin'] = val;
+            else if (key == "person") filter['createdBy.email'] = val; 
+        }
+    }
 
     const notifications = await notificationService.getAllNotifications(orgId, filter, page, limit);
 
