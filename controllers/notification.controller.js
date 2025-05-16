39b7dfb9b836897a7e41764d41200a24f111ed0d
pyloris <shoaibwani@protonmail.com>
2025-05-16 07:16:11 +0200

    fixed the notification controller

--- controllers/notification.controller.js
+++ controllers/notification.controller.js
@@ -2,13 +2,15 @@ const notificationService = require("../services/notification.service");
 
 
 const getAllNotifications = async (req, res) => {
-    const { orgId } = req.authenticatedService;
+    const { orgId, email } = req.authenticatedService;
 
     const { page=1, limit=10, origin } = req.query;
 
     const filter = {};
 
     if (origin && origin != "all") filter.origin = origin;
+
+    filter['createdBy.email'] = email;
 
     const notifications = await notificationService.getAllNotifications(orgId, filter, page, limit);
 
