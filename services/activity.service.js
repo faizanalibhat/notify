823ec4c5ea1d05eb5c30b7b21dd572ec766a214b
Shoaib Wani <wanishoaib@protonmail.com>
2025-09-17 15:36:50 +0530

    added activities for risk regsiter as well

--- services/activity.service.js
+++ services/activity.service.js
@@ -123,7 +123,33 @@ const endpointWithMethodActionMap = {
     },
     '//api/integrations/[0-9a-z]+': {
         'DELETE': "Removed an Adapter from AIM"
-    }
+    },
+
+
+    '/risk-register/api/v1/risks': {
+        'GET': 'Listed all risks in risk register',
+        'POST': 'Created a new risk in risk register',
+    },
+    '/risk-register/api/v1/risks/[0-9a-z]+': {
+        'PATCH': 'Updated a risk in the risk register',
+        'DELETE': 'Deleted a risk in the risk register',
+    },
+    '/risk-register/api/v1/frameworks': {
+        'GET': 'Listed frameworks in the risk register',
+        'POST': 'Created a new framework in risk register',
+    },
+    '/risk-register/api/v1/frameworks/[0-9a-z]+': {
+        'PATCH': 'Updated a framework in the risk register',
+        'DELETE': 'Deleted a framework in risk register',
+    },
+    '/risk-register/api/v1/frameworks/[0-9a-z]+/controls': {
+        'GET': 'Listed controls of a framework in risk register',
+        'POST': 'Created a new control in a framework in risk register',
+    },
+    '/risk-register/api/v1/frameworks/[0-9a-z]+/controls/[0-9a-z]+': {
+        'PATCH': 'Updated a control in a framework in the risk register',
+        'DELETE': 'Deleted a control in a framework in the risk register',
+    },
 }
 
 
