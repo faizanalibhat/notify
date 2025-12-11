2c3d3093712525080b5192a444611d3e18604a5f
Alpha <syedaarishshah@gmail.com>
2025-12-11 15:21:29 +0530

    remove ttl, as we will later set it to cron

--- models/activityLog.model.js
+++ models/activityLog.model.js
@@ -4,7 +4,4 @@ const activityLogSchema = new mongoose.Schema({
     payload: { type: mongoose.Schema.Types.Mixed }
 }, { timestamps: true });
 
-// Set a TTL index to automatically expire documents after 30 days (2592000 seconds)
-activityLogSchema.index({ createdAt: 1 }, { expireAfterSeconds: 2592000 });
-
 module.exports = mongoose.model("activity_log", activityLogSchema);
