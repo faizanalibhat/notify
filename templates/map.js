5a44cc9443a1677ac8e3d1f3fbfba1e318e7de8b
Alpha <syedaarishshah@gmail.com>
2025-12-22 15:54:12 +0530

    htm mpping

---
+++ templates/map.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        template_id: "LOGIN_OTP",
+        template_path: "/templates/email/LOGIN_OTP.hbs",
+        active: true,
+        type: "email"
+    },
+    {
+        template_id: "MAGIC_LINK",
+        template_path: "/templates/email/MAGIC_LINK.hbs",
+        active: true,
+        type: "email"
+    },
+    {
+        template_id: "PASSWORD_RESET",
+        template_path: "/templates/email/PASSWORD_RESET.hbs",
+        active: true,
+        type: "email"
+    },
+    {
+        template_id: "REGISTRATION_OTP",
+        template_path: "/templates/email/REGISTRATION_OTP.hbs",
+        active: true,
+        type: "email"
+    },
+    {
+        template_id: "ORG_INVITATION",
+        template_path: "/templates/email/ORG_INVITATION.hbs",
+        active: true,
+        type: "email"
+    },
+    {
+        template_id: "GENERIC_EMAIL",
+        template_path: "/templates/email/GENERIC_EMAIL.hbs",
+        active: true,
+        type: "email"
+    }
+];
