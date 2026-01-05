50ece435eef6aea9f0d5c172ab9e7a55c9ccfd35
Shoaib Wani <wanishoaib@protonmail.com>
2026-01-05 13:36:37 +0530

    fixed cors

--- app.js
+++ app.js
@@ -7,38 +7,42 @@ const router = require("./routes/index");
 const cors = require("cors");
 const { authenticateService } = require("./middlewares/auth");
 
-
 // workers
 const emailWorker = require("./workers/email/email.worker");
 const activityWorker = require("./workers/activity/worker");
 const notificationWorker = require("./workers/notification.worker");
 
-
 const app = express();
 
 app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+
 app.use(bodyParser.json());
 
 app.use("/notify/api", authenticateService(), router);
 
 // connect mongodb
-mongoose.connect(process.env.NOTIFY_MONGODB_URL)
-.then(() => {
+mongoose
+  .connect(process.env.NOTIFY_MONGODB_URL)
+  .then(() => {
     console.log("[+] MONGODB CONNECTED");
-})
-.catch((err) => {
+  })
+  .catch((err) => {
     console.log("[-] FAILED TO CONNECT MONGODB ", err.message);
-});
-
+  });
 
 // run the email worker
 emailWorker();
 activityWorker();
 notificationWorker();
 
-
 app.use(errorHandler);
 
 app.listen(process.env.NOTIFY_PORT || 80, () => {
-    console.log("[+] SERVER IS RUNNING ON PORT ", process.env.NOTIFY_PORT || 80);
+  console.log("[+] SERVER IS RUNNING ON PORT ", process.env.NOTIFY_PORT || 80);
 });
