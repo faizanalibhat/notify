35ca144d9a2c396bfde572f2f64ee781a47f18a7
Shoaib Wani <wanishoaib@protonmail.com>
2026-02-18 23:02:01 +0530

    added service urls

--- config/app.config.js
+++ config/app.config.js
@@ -9,6 +9,19 @@ const appConfig = {
     SMTP_USER: Config.get("SMTP_USER"),
     SMTP_PASS: Config.get("SMTP_PASS"),
     EMAIL_FROM: Config.get("EMAIL_FROM"),
+
+    // Standard Service URLs
+    AUTH_SERVICE_URL: Config.get("AUTH_SERVICE_URL", "http://auth"),
+    AIM_SERVICE_URL: Config.get("AIM_SERVICE_URL", "http://aim"),
+    VM_SERVICE_URL: Config.get("VM_SERVICE_URL", "http://vm"),
+    UTIL_SERVICE_URL: Config.get("UTIL_SERVICE_URL", "http://util"),
+    OP_SERVICE_URL: Config.get("OP_SERVICE_URL", "http://op"),
+    VS_SERVICE_URL: Config.get("VS_SERVICE_URL", "http://vs"),
+    VSCANNER_SERVICE_URL: Config.get("VSCANNER_SERVICE_URL", "http://vscanner"),
+    CMDEXEC_SERVICE_URL: Config.get("CMDEXEC_SERVICE_URL", "http://cmdexec"),
+    TM_SERVICE_URL: Config.get("TM_SERVICE_URL", "http://tm"),
+    ASSET_INVENTORY_SERVICE_URL: Config.get("ASSET_INVENTORY_SERVICE_URL", "http://asset-inventory"),
+    NOTIFY_SERVICE_URL: Config.get("NOTIFY_SERVICE_URL", "http://notify"),
 }
 
 
