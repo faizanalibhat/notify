98c676f6051dd91d82a751c3f3d114de2149ea3f
Shoaib Wani <wanishoaib@protonmail.com>
2026-03-03 16:24:30 +0530

    added swagger.json

--- swagger/index.js
+++ swagger/index.js
@@ -2,5 +2,10 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerSpec = require('./swagger.config')
 
 module.exports = (app) => {
+    app.get("/notify/swagger.json", (req, res) => {
+        res.setHeader("Content-Type", "application/json");
+        res.send(swaggerSpec);
+    });
+
     app.use('/notify/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 }
